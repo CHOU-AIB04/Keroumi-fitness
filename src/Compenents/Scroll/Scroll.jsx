1eda4396d81f0e6609ab1bb18dce40917c5c4d3c
CHOU-AIB04 <142614891+CHOU-AIB04@users.noreply.github.com>
2024-02-05 14:33:30 +0100

    fix scroll class

--- src/Compenents/Scroll/Scroll.jsx
+++ src/Compenents/Scroll/Scroll.jsx
@@ -19,7 +19,7 @@ export const Scroll = () => {
         {Scroll ? 
             <div className='w-10 h-10 bg-orange-500 rounded-full sh fixed bottom-10 right-10 small:right-12 flex justify-center items-center cursor-pointer z-40' onClick={scroll}>
                 {
-                  location.pathname !== "/Keroumi-V1/Protein" ? <i className="bi bi-arrow-up text-white font-bold"></i> : <i className="bi bi-chevron-double-up text-white font-bold"></i>
+                location.pathname === "/Keroumi-V1/Protein/Product-details" || location.pathname === "/Keroumi-V1/Protein" || location.pathname === "/keroumi-V1/Protein/Card"  ?  <i className="bi bi-chevron-double-up text-white font-bold"></i> : <i className="bi bi-arrow-up text-white font-bold"></i>
                 }
             </div> 
             : 
