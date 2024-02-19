4cae7980f7446634de66bc85ae08360ccc0e2561
CHOU-AIB04 <142614891+CHOU-AIB04@users.noreply.github.com>
2024-02-19 17:04:03 +0100

    fix footer prob and scroll y

--- src/Compenents/Footer/Footer.jsx
+++ src/Compenents/Footer/Footer.jsx
@@ -13,22 +13,22 @@ const Footer = (props) => {
 
   return (
     <footer className={`relative ${props.footer_responsive} h-56 flex flex-col items-center justify-between w-full`}>
-        <Link to="Keroumi-V1/">
+        <Link to="Keroumi-fitness/">
         <motion.div initial={{opacity:0}} transition={{duration:1}} whileInView={{opacity:1}} viewport={{once:true}} className='w-52 small:w-72 relative' onClick={Hidenav}>
             <img src={logo} alt="picture" />
         </motion.div>
         </Link>
         <motion.ul viewport={{once:true}} initial={{opacity:0}} transition={{duration:1}} whileInView={{opacity:1}} className='text-white w-[350px] justify-evenly small:justify-between items-center flex relative'>
-            <NavLink to="Keroumi-V1/" end onClick={Hidenav} style={({isActive})=> isActive ? style: null}>
+            <NavLink to="Keroumi-fitness/" end onClick={Hidenav} style={({isActive})=> isActive ? style: null}>
                 <li className='transition-all duration-500 hover:text-orange-600 cursor-pointer'>Home</li>
             </NavLink>
-            <NavLink to="Keroumi-V1/Contact" end onClick={Hidenav} style={({isActive})=> isActive ? style: null}>
+            <NavLink to="Keroumi-fitness/Contact" end onClick={Hidenav} style={({isActive})=> isActive ? style: null}>
                 <li className='transition-all duration-500 hover:text-orange-600 cursor-pointer'>Contact Us</li>
             </NavLink>
-            <NavLink to="Keroumi-V1/About" end onClick={Hidenav} style={({isActive})=> isActive ? style: null}>
+            <NavLink to="Keroumi-fitness/About" end onClick={Hidenav} style={({isActive})=> isActive ? style: null}>
                 <li className='transition-all duration-500 hover:text-orange-600 cursor-pointer'>About Us</li>
             </NavLink>
-            <NavLink to="Keroumi-V1/Store" end onClick={Hidenav} style={({isActive})=> isActive ? style: null}>
+            <NavLink to="Keroumi-fitness/Store" end onClick={Hidenav} style={({isActive})=> isActive ? style: null}>
                 <li className='transition-all duration-500 hover:text-orange-600 cursor-pointer'>Protein<span className='ml-1'>Store</span></li>
             </NavLink>     
         </motion.ul>
